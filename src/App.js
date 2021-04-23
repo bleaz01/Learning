58e0da7423366c56302cddb9b99a485ea2d5a7c4
jeason <ridjea@gmail.com>
2021-04-23 21:24:37 +0200

    no message

--- src/App.js
+++ src/App.js
@@ -1,88 +1,88 @@
-import React, { Suspense, lazy ,useState, useEffect} from 'react';
-import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
-import {useDispatch, useSelector} from "react-redux"
-import useAxios from 'axios-hooks'
-import useAuthentication from "./lib/hooks/useAuthent"
+import React, { Suspense, lazy, useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useLocation
+} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import useAxios from "axios-hooks";
+import useAuthentication from "./lib/hooks/useAuthent";
 
-
-import './App.scss';
-import WelcomePage from './Components/layout/WelcomePage/WelcomePage';
-import NavBar from './Components/NavBar/NavBar';
-import Profile from './Components/routes/Profile/Profile';
-import  Chat  from './Components/Chat/Chat';
-import MyImg from './assets/jeason.jpg';
-import SideBar from './Components/layout/ModalSidebar/SideBar';
+import "./App.scss";
+import WelcomePage from "./Components/layout/WelcomePage/WelcomePage";
+import NavBar from "./Components/NavBar/NavBar";
+import Profile from "./Components/routes/Profile/Profile";
+import Chat from "./Components/Chat/Chat";
+import MyImg from "./assets/jeason.jpg";
+import SideBar from "./Components/layout/ModalSidebar/SideBar";
 
 //Routes
 
-const Home = lazy(() => import('./Components/routes/Home/Home'));
-const Games = lazy(() => import('./Components/routes/Games/Games'));
-const Verbs = lazy(() => import('./Components/routes/Verbs/Verbs'));
-const Vocabulary = lazy(() => import('./Components/routes/Vocabulary/Vocabulary'));
-const Messenger = lazy(() => import('./Components/routes/Messenger/Messenger'));
-
+const Home = lazy(() => import("./Components/routes/Home/Home"));
+const Games = lazy(() => import("./Components/routes/Games/Games"));
+const Verbs = lazy(() => import("./Components/routes/Verbs/Verbs"));
+const Vocabulary = lazy(() =>
+  import("./Components/routes/Vocabulary/Vocabulary")
+);
+const Messenger = lazy(() => import("./Components/routes/Messenger/Messenger"));
 
 function App() {
+  const dispatch = useDispatch();
+  const { handleAuthentication } = useAuthentication(dispatch);
 
-
-  const dispatch = useDispatch()
-  const { handleAuthentication } = useAuthentication(dispatch)
-
-  useEffect(()=>{
+  useEffect(() => {
     handleAuthentication();
-  },[])
+  }, []);
 
-  const user = useSelector((state) => state.user)
-  console.log(user.user, "llouise")
-  const [logged, setLogged] = useState(false)
+  const user = useSelector(state => state.user);
+  console.log(user.user, "llouise");
+  const [logged, setLogged] = useState(false);
 
   return (
-
     <div className="App">
-      {
-      user? 
-      // logged?
-      <>
-      <Router>
-        <header className="App-header">
-          <NavBar user={user}/>
-        </header>
-        <div className="App-main">
-          <div className="App-container">
-            <Suspense fallback={<div>Chargement...</div>}>
-              <Switch>
-                <Route exact path={"/home"}>
-                  <Home/>
-                </Route>
-                <Route path="/spelletjes">
-                  <Games/>
-                </Route>
-                <Route path="/verbum">
-                  <Verbs/>
-                </Route>
-                <Route path="/vocabulary">
-                  <Vocabulary/>
-                </Route>
-                <Route path="/messenger">
-                 <Messenger />
-                </Route>
-                <Route path ="/Profile">
-                  <Profile />
-                </Route>
-              </Switch>
-            </Suspense> 
-          </div>
-        </div>
-        <footer className="App-footer">
-          <p> © 2020-2021 Riguelle Jeason </p>
-        </footer>
-      </Router>
-      </>
-     :
-     <WelcomePage/>
-    }  
+      {user ? (
+        // logged?
+        <>
+          <Router>
+            <header className="App-header">
+              <NavBar user={user} />
+            </header>
+            <div className="App-main">
+              <div className="App-container">
+                <Suspense fallback={<div>Chargement...</div>}>
+                  <Switch>
+                    <Route exact path={"/home"}>
+                      <Home />
+                    </Route>
+                    <Route path="/spelletjes">
+                      <Games />
+                    </Route>
+                    <Route path="/verbum">
+                      <Verbs />
+                    </Route>
+                    <Route path="/vocabulary">
+                      <Vocabulary />
+                    </Route>
+                    <Route path="/messenger">
+                      <Messenger />
+                    </Route>
+                    <Route path="/Profile">
+                      <Profile />
+                    </Route>
+                  </Switch>
+                </Suspense>
+              </div>
+            </div>
+            <footer className="App-footer">
+              <p> © 2020-2021 Riguelle Jeason </p>
+            </footer>
+          </Router>
+        </>
+      ) : (
+        <WelcomePage />
+      )}
     </div>
-     
   );
 }
 
--- src/Components/Forms/RegisterForm/RegisterForm.js
+++ src/Components/Forms/RegisterForm/RegisterForm.js
@@ -1,70 +1,69 @@
-import React from 'react'
+import React from "react";
 import { useForm } from "react-hook-form";
-import {useDispatch} from 'react-redux';
+import { useDispatch } from "react-redux";
 
-import Button from '../../Item/Button/Button';
-import Input from '../../Item/Input/Input';
-import useAuthentication from "../../../lib/hooks/useAuthent"
-import GoogleBtn from "./GoogleBtn"
+import Button from "../../Item/Button/Button";
+import Input from "../../Item/Input/Input";
+import useAuthentication from "../../../lib/hooks/useAuthent";
+import GoogleBtn from "./GoogleBtn";
 
-import {useMutation} from '@apollo/client'
-import { ADD_USER } from '../../../lib/apollo/mutation'
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../../../lib/apollo/mutation";
 
+import "./RegisterFormStyleSheet.scss";
 
-import "./RegisterFormStyleSheet.scss"
+const RegisterForm = () => {
+  const dispatch = useDispatch();
 
-const RegisterForm = ()=>{
-    const dispatch = useDispatch()
+  const { register, handleSubmit, watch, errors } = useForm();
+  const { handleUserRegistration } = useAuthentication(dispatch);
+  // const [addUser, { data }] = useMutation(ADD_USER);
 
+  const onSubmit = data => {
+    // console.log(data);
+    const use = data;
 
-    const { register, handleSubmit, watch, errors } = useForm();
-    const {handleUserRegistration} = useAuthentication(dispatch)
-    // const [addUser, { data }] = useMutation(ADD_USER);
+    handleUserRegistration(use).then(() => {
+      // addUser({ variables: { email: data.email, password: data.password } });
+      console.log("successsfuly");
+    });
+  };
 
-    const onSubmit = data =>  {
-
-        console.log(data)
-        // const use = data
-
-        // handleUserRegistration(use).then(()=>{
-        //     // addUser({ variables: { email: data.email, password: data.password } });
-        //     console.log("successsfuly")
-        // })
-
-    }
-
-    return(
-        
-        <div className='RegisterForm-Container'>
-            <div className='RegisterForm-Title'>
-              <h2>Register</h2>
-            </div>
-           <form className='RegisterForm-Form' onSubmit={handleSubmit(onSubmit)}>
-                <Input
-                    placeholder="email"
-                    name="email" 
-                    // defaultValue={defaultValue && 'test'} 
-                    inputRef={register} 
-                    />
-                <Input 
-                    placeholder="password"
-                    name="password" 
-                    // defaultValue={defaultValue && 'test'} 
-                    inputRef={register} 
-                />
-                 <select className='FormBase-input' name="role" ref={register}>
+  return (
+    <div className="RegisterForm-Container">
+      <div className="RegisterForm-Title">
+        <h2>Register</h2>
+      </div>
+      <form className="RegisterForm-Form" onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          placeholder="email"
+          name="email"
+          // defaultValue={defaultValue && 'test'}
+          inputRef={register}
+        />
+        <Input
+          placeholder="password"
+          name="password"
+          // defaultValue={defaultValue && 'test'}
+          inputRef={register}
+        />
+        {/* <select className='FormBase-input' name="role" ref={register}>
                     <option value="student">Student</option>
                     <option value="teacher">Teacher</option>
-                 </select>
-                 <Button buttonStyles="color-white" buttonSize="small" onclick={onSubmit}>Register</Button> 
+                 </select> */}
+        <Button
+          buttonStyles="color-white"
+          buttonSize="small"
+          onclick={onSubmit}
+        >
+          Register
+        </Button>
+      </form>
+      <div style={{ margin: "20px" }}>
+        <GoogleBtn />
+      </div>
+    </div>
+  );
+};
 
-            </form>  
-            <div style={{margin:"20px"}}>
-                <GoogleBtn/>
-            </div>
-        </div>
-
-    )
-}
-
-export default RegisterForm
+export default RegisterForm;
